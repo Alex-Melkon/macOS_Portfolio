1bc167e55d6a36e1b79d86d35f83bc20d26785cd
Alex-Melkon <97638853+Alex-Melkon@users.noreply.github.com>
2026-01-18 16:12:13 -0500

    implement code suggestions from Senior overview

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -22,7 +22,7 @@ return [...text].map((char, i) => (
 }
 
 const setupTextHover = (container, type ) => {
-    if(!container) return;
+    if(!container) return () => {} ;
 
     const letters = container.querySelectorAll("span");
     const {min, max, default: base } = FONT_WEIGHT[type];
